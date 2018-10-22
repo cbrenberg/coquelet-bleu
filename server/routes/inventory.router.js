d3e9bcebfc57f9049327d53078e1231e3dd0333c
Christopher Brenberg <cbrenberg@gmail.com>
2018-10-22 11:31:57 -0500

    create server routes for customer orders and admin inventory and orders

--- server/routes/inventory.router.js
+++ server/routes/inventory.router.js
@@ -4,10 +4,19 @@ const router = express.Router();
 
 router.post('/', (req, res) => {
   console.log('req.body:', req.body);
+  pool.query(`INSERT INTO "beans" ("name", "origin_description", "flavor_description", "image_url", "quantity")
+              VALUES ($1, $2, $3, $4, $5);`,
+              [req.body.name, req.body.origin_description, req.body.flavor_description, req.body.image_url, req.body.quantity])
+    .then(() => res.sendStatus(201))
+    .catch(error => {
+      console.log('Error submitting order', error);
+      res.sendStatus(500);
+    })
 })
 
+//UPDATE "beans" table from admin inventory table
 router.put('/', (req, res) => {
-  console.log(req.body);
+  console.log('/api/inventory PUT hit');
   pool.query(`UPDATE "beans" SET "$1"='$2' WHERE "id"=$3;`, [req.body.column, req.body.value, req.body.id])
     .then(() => {
       res.sendStatus(201);
@@ -15,14 +24,15 @@ router.put('/', (req, res) => {
     .catch(error => console.log('Error editing item:', error))
 })
 
+//GET all in stock inventory for admin table display
 router.get('/', (req, res) => {
-  console.log('/orders GET hit');
+  console.log('/api/inventory GET hit');
   pool.query(`SELECT outerBean."id", outerBean."flavor_description", outerBean."image_url", outerBean."name", outerBean."origin_description", outerBean."quantity", 
-	(SELECT array_agg("roast_levels"."roast") as "roasts" FROM "roast_levels"
-		JOIN "roast_junction" ON "roast_junction"."roast_id"="roast_levels"."id"
-		JOIN "beans" ON "roast_junction"."bean_id" = "beans"."id"
-		WHERE "roast_junction"."bean_id" = outerBean."id")
-FROM "beans" outerBean;`)
+                (SELECT array_agg("roast_levels"."roast") as "roasts" FROM "roast_levels"
+                  JOIN "roast_junction" ON "roast_junction"."roast_id"="roast_levels"."id"
+                  JOIN "beans" ON "roast_junction"."bean_id" = "beans"."id"
+                  WHERE "roast_junction"."bean_id" = outerBean."id")
+              FROM "beans" outerBean;`)
     .then(results => {
       console.log(results.rows);
       res.send(results.rows);
@@ -33,8 +43,24 @@ FROM "beans" outerBean;`)
     })
 })
 
+//GET all roast levels to populate dropdown for customer
+router.get('/roasts', (req, res) => {
+  console.log('/api/inventory/roasts GET hit');
+  pool.query(`SELECT * FROM "roast_levels"`)
+    .then( results => {
+      res.send(results.rows);
+    })
+    .catch(error => {
+      console.log('Error getting roast levels', error);
+      res.sendStatus(500);
+    })
+})
+
 router.delete('/', (req, res) => {
-  console.log('/feedback DELETE hit. req.query:', req.query);
+  console.log('/api/inventory DELETE hit. req.query:', req.query);
+  pool.query(`DELETE FROM "orders" WHERE "id"=$1;`, [req.query.id])
+    .then(() => res.sendStatus(200))
+    .catch(error => console.log('Error deleting order', error))
 })
 
 module.exports = router;
--- server/routes/orders.router.js
+++ server/routes/orders.router.js
@@ -4,10 +4,18 @@ const router = express.Router();
 
 router.post('/', (req, res) => {
   console.log('req.body:', req.body);
+  pool.query(`INSERT INTO "orders" ("first_name", "last_name", "street_address", "city", "state", "zipcode", "phone", "email", "bean", "roast", "order_status", "quantity")
+              VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12);`, 
+              [req.body.first_name, req.body.last_name, req.body.street_address, req.body.city, req.body.state, req.body.zipcode, req.body.phone, req.body.email, req.body.bean, req.body.roast, req.body.order_status, req.body.quantity])
+    .then(() => res.sendStatus(201))
+    .catch(error => {
+      console.log('Error submitting order', error);
+      res.sendStatus(500);
+    })
 })
 
 router.get('/', (req, res) => {
-  console.log('/orders GET hit');
+  console.log('/api/orders GET hit');
   pool.query(`SELECT "orders"."id",
               "orders"."first_name", 
               "orders"."last_name", 
@@ -31,6 +39,16 @@ router.get('/', (req, res) => {
     })
     .catch(error => {
       console.log('Error getting order data', error);
+      res.sendStatus(500);
+    })
+})
+
+router.put('/', (req, res) => {
+  console.log('/api/orders PUT hit');
+  pool.query(`UPDATE "orders" SET "order_status"=$1 WHERE "id"=$2`, [req.body.order_status, req.body.id])
+    .then(() => res.sendStatus(201))
+    .catch(error => {
+      console.log('Error editing order status', error);
       res.sendStatus(500);
     })
 })
