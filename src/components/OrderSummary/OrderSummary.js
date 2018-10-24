60fbe267835bc4c2b9a0db65d9878dcb0b46ba3f
Christopher Brenberg <cbrenberg@gmail.com>
2018-10-24 14:57:08 -0500

    display selected quantity in order summary after being selected

--- src/components/OrderSummary/OrderSummary.js
+++ src/components/OrderSummary/OrderSummary.js
@@ -9,7 +9,12 @@ class OrderSummary extends Component {
   render() {
     //takes just the selected bean property from redux store
     const currentOrder = this.props.toDisplay;
-    if (this.props.toDisplay.bean && !this.props.toDisplay.roast) {
+    if (!this.props.toDisplay.bean && !this.props.toDisplay.roast && !this.props.toDisplay.quantity) {
+      return (
+        // <pre>{JSON.stringify(this.props, null, 2)}</pre>
+        <h5>Make some choices, already!</h5>
+      )
+    } else if (this.props.toDisplay.bean && !this.props.toDisplay.roast && !this.props.toDisplay.quantity) {
       return (
         <div className="container">
           <Image
@@ -23,10 +28,9 @@ class OrderSummary extends Component {
               <li><strong>Suggested Roasts: </strong>{currentOrder.bean.roasts.join(', ')}</li>
             </ul>
           </div>
-
         </div>
       )
-    } else if (this.props.toDisplay.roast) {
+    } else if (this.props.toDisplay.bean && this.props.toDisplay.roast && !this.props.toDisplay.quantity) {
       return (
         <div className="container">
           <Image
@@ -41,13 +45,25 @@ class OrderSummary extends Component {
               <li><strong>Roast Description: </strong>{currentOrder.roast.description}</li>
             </ul>
           </div>
-
         </div>
       )
     } else {
       return (
-        // <pre>{JSON.stringify(this.props, null, 2)}</pre>
-        <h5>Make some choices, already!</h5>
+        <div className="container">
+          <Image
+            src={currentOrder.bean.image_url}
+          />
+          <div className="textDiv">
+            <ul id="orderSummaryList">
+              <li><strong>Selected Bean: </strong>{currentOrder.bean.name}</li>
+              <li><strong>Description: </strong>{currentOrder.bean.origin_description}</li>
+              <li><strong>Flavor Notes: </strong>{currentOrder.bean.flavor_description}</li>
+              <li><strong>Selected Roast: </strong>{currentOrder.roast.roast}</li>
+              <li><strong>Roast Description: </strong>{currentOrder.roast.description}</li>
+              <li><strong>Quantity: </strong>{currentOrder.quantity} oz.</li>
+            </ul>
+          </div>
+        </div>
       )
     }
   }
