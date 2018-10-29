42c2f0796c01482ba1258184aa558bab30b7bc4d
Christopher Brenberg <cbrenberg@gmail.com>
2018-10-29 13:49:19 -0500

    new logo with transparent background, homepage styling

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './HomePage.css';
-import RoosterLogo from '../../images/rooster-logo-blue.png';
+import RoosterLogoSansBG from '../../images/rooster-logo-transparent.png';
 import { withRouter } from 'react-router-dom';
 
 // This is one of our simplest components
@@ -10,28 +10,29 @@ import { withRouter } from 'react-router-dom';
 
 const HomePage = withRouter((props) => (
   <div id="homePage">
-      <h1>Welcome</h1>
-      <div className="container">
-        <div className="imageDiv">
-          <img id="logo" src={RoosterLogo} />
-        </div>
-        <div className="textDiv">
-          <p>Coquelet Bleu Coffee roasts coffee beans to your exact specifications, one tiny batch at a time.</p>
-          <h3>Here's how it works:</h3>
-          <ol>
-            <li>Select your beans: Choose from any of the beans currently in stock.</li>
-            <li>Select your roast: From light to dark, we've got you covered.</li>
-            <li>Select your quantity: Up to one pound at a time.</li>
-            <li>Pay securely with Stripe</li>
-            <li>Await delivery: Your order will arrive when your beans are at their peak of flavor</li>
-          </ol>
-          <button 
-            onClick={(event) => {
-              event.preventDefault();
-              props.history.push('/order');
-            }}>Click Here To Get Started</button>
-        </div>
+    <div className="container">
+      <div className="imageDiv">
+        <h1>Welcome</h1>
+        <img id="logo" src={RoosterLogoSansBG} />
       </div>
+      <div className="textDiv">
+        <p>Coquelet Bleu Coffee roasts coffee beans to your exact specifications, one tiny batch at a time.</p>
+        <h3>Here's how it works:</h3>
+        <ol>
+          <li>Select your beans: Choose from any of the beans currently in stock.</li>
+          <li>Select your roast: From light to dark, we've got you covered.</li>
+          <li>Select your quantity: Up to one pound at a time.</li>
+          <li>Pay securely with Stripe</li>
+          <li>Await delivery: Your order will arrive when your beans are at their peak of flavor</li>
+          <li>Enjoy!</li>
+        </ol>
+        <button
+          onClick={(event) => {
+            event.preventDefault();
+            props.history.push('/order');
+          }}>Click Here To Get Started</button>
+      </div>
+    </div>
   </div>
 ));
 
