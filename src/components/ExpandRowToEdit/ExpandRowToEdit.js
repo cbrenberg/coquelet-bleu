473f2a866d5b4402b15849d4dcfc0a6b9a5e42b9
Christopher Brenberg <cbrenberg@gmail.com>
2018-10-29 21:14:21 -0500

    rearrange inventory edit form

--- src/components/ExpandRowToEdit/ExpandRowToEdit.js
+++ src/components/ExpandRowToEdit/ExpandRowToEdit.js
@@ -23,29 +23,34 @@ class ExpandRowToEdit extends Component {
     return (
       <form className="editInventory" onSubmit={this.submitForm}>
         <div className="inputsDiv">
-          <label>Origin:
+          <div className="inputsDivChild">
+            <label>Origin:
             <input name='name' value={this.state.name} onChange={this.handleChange} />
-          </label>
-          <label>Description:
+            </label>
+            <label>Description:
             <textarea rows="5" name='origin_description' value={this.state.origin_description} onChange={this.handleChange} />
-          </label>
-          <label>Quantity (oz.):
+            </label>
+            <label>Quantity (oz.):
             <input name='quantity' value={this.state.quantity} onChange={this.handleChange} />
-          </label>
-          <label>Image URL:
+            </label>
+            <label>Image URL:
             <input name='image_url' value={this.state.image_url} onChange={this.handleChange} />
-          </label>
-          <label>Flavor Description:
+            </label>
+          </div>
+          <div className="inputsDivChild">
+            <label>Flavor Description:
             <textarea rows="5" name='flavor_description' value={this.state.flavor_description} onChange={this.handleChange} />
-          </label>
-          <label> Other Notes (not public):
+            </label>
+            <label> Other Notes (not public):
             <textarea rows="5" name='notes' value={this.state.notes} onChange={this.handleChange} />
-          </label>
+            </label>
+            <button id="editInventorySubmit" type="submit">Submit Changes</button>
+          </div>
         </div>
-        <button className="editInventorySubmit" type="submit">Submit Changes</button>
+
 
         {/* <pre>{JSON.stringify(this.state)}</pre> */}
-      </form>
+      </form >
     )
   }
 }
--- src/components/Image/Image.js
+++ src/components/Image/Image.js
@@ -3,11 +3,6 @@ import { connect } from 'react-redux';
 import RoosterLogo from '../../images/rooster-logo-blue.png';
 import './Image.css';
 
-// This is one of our simplest components
-// It doesn't have local state, so it can be a function component.
-// It doesn't dispatch any redux actions or display any part of redux state
-// or even care what the redux state is, so it doesn't need 'connect()'
-
 class Image extends Component {
   state = { loaded: false };
 
