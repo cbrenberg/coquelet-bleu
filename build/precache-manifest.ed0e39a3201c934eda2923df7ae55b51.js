02e938e842f1f7c2db1a9be6bbe2e601ae81964b
Christopher Brenberg <cbrenberg@gmail.com>
2018-11-07 17:24:41 -0600

    add build files for heroku and update database.sql

---
+++ build/precache-manifest.ed0e39a3201c934eda2923df7ae55b51.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "36030f450f76b0e5d21d",
+    "url": "/static/css/main.50774841.chunk.css"
+  },
+  {
+    "revision": "36030f450f76b0e5d21d",
+    "url": "/static/js/main.36030f45.chunk.js"
+  },
+  {
+    "revision": "5fef5606d1f62192b0b1",
+    "url": "/static/css/1.4867a9c0.chunk.css"
+  },
+  {
+    "revision": "5fef5606d1f62192b0b1",
+    "url": "/static/js/1.5fef5606.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4c78fec2cdd713119cc68165b79528de",
+    "url": "/static/media/rooster-logo-bw.4c78fec2.png"
+  },
+  {
+    "revision": "d3ec48c1cc7d9eb3ad1efbc6571bd4c9",
+    "url": "/static/media/rooster-logo-blue.d3ec48c1.png"
+  },
+  {
+    "revision": "4bf0e3b8c5a2fcdb5a868ce286d407ed",
+    "url": "/static/media/arrow-up-circle.4bf0e3b8.svg"
+  },
+  {
+    "revision": "b2066acf199315ed7da9990cf5f9150f",
+    "url": "/static/media/arrow-down-circle.b2066acf.svg"
+  },
+  {
+    "revision": "d17740f00a4bd718ac15a8ff1bccc8d0",
+    "url": "/static/media/rooster-logo-transparent.d17740f0.png"
+  },
+  {
+    "revision": "f0e83cb823af985e89d1ff48b92b5d8b",
+    "url": "/static/media/more-horizontal.f0e83cb8.svg"
+  },
+  {
+    "revision": "a7e99a4f6a4acea2d7055294a4561adb",
+    "url": "/static/media/edit.a7e99a4f.svg"
+  },
+  {
+    "revision": "b0200bf7cbbb26c32989e7a0e4d19eef",
+    "url": "/static/media/x.b0200bf7.svg"
+  },
+  {
+    "revision": "3ae13bc863b5aebd90257119aa4b22f0",
+    "url": "/static/media/FacebookIcon.3ae13bc8.svg"
+  },
+  {
+    "revision": "6806419b7b1e42c1f877c1d41a7f4abe",
+    "url": "/static/media/coffee-background.6806419b.jpg"
+  },
+  {
+    "revision": "9e8c8927b36ff1a727fe3147d50d2369",
+    "url": "/index.html"
+  }
+];
